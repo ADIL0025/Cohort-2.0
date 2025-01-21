ba282f1a77a88d69bca8ef00a9399a68f11dab52
Adil Ahmad <adilahmad0025@gmail.com>
2025-01-21 19:42:00 +0530

    Refactor asynchronous function examples and add a sample text file

--- Week 1/1.4/Asynchronous_func.js
+++ Week 1/1.4/Asynchronous_func.js
@@ -1,14 +1,35 @@
-function findSum(n){
-    let ans = 0;
-    for(let i = 0; i< n; i++){
-        ans = ans + i;
-    }
-    return ans;
+// function findSum(n){
+//     let ans = 0;
+//     for(let i = 0; i< n; i++){
+//         ans = ans + i;
+//     }
+//     return ans;
+// }
+
+// function findSumTill100(){
+//     console.log(findSum(100));
+// }
+
+// setTimeout(findSumTill100, 1000) // calling a async function
+// console.log("Hello World");
+
+/* 
+--------------------------->Common async functions are
+
+ ----- (i) setTimeout() , fsreadFile(), fetch()
+*/
+
+const fs = require("fs");
+// fileSystem module
+
+fs.readFile("a.txt", "utf-8", function(err, data){  // running in replit
+    console.log(data); //3rd run
+})
+
+console.log("Hello World"); // 1st run
+let a = 0;
+for(let i = 0; i < 10000000; i++){
+    a = a + i;
 }
 
-function findSumTill100(){
-    console.log(findSum(100));
-}
-
-setTimeout(findSumTill100, 1000) // calling a async function
-console.log("Hello World");
+console.log("Hi there 2"); // 2nd run
